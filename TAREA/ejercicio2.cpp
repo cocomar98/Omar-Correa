dcd27325285332d78b538a8feee462d2bf8651d4
cocomar98 <60759983+cocomar98@users.noreply.github.com>
2020-02-06 23:29:37 -0600

    Add files via upload

---
+++ TAREA/ejercicio2.cpp
@@ -0,0 +1,14 @@
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	int N;
+	
+	cout<<"Ordenar los numeros del 0 al -99  "<<N<<endl;
+	cin>>N;
+	cout<<" EL numero mayor negativo es  "<<N<<endl;
+
+	return 0;
+
+}
